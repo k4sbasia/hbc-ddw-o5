25ee7d4ce707c7f6cef45b17936d75fdc5b9d160
alexandragcozma <60522131+alexandragcozma@users.noreply.github.com>
2020-03-20 21:19:32 +0200

    Update o5_Simons_product_feed.sh

--- o5_Simons_product_feed.sh
+++ o5_Simons_product_feed.sh
@@ -58,26 +58,26 @@ echo "bi_partner_base Extract  Process started at `date '+%a %b %e %T'`" >${LOG_
 #  Run the sql script that performs the product info extract
 #################################################################
 echo "bi_partner_base Sdw  Process started at `date '+%a %b %e %T'`" >>${LOG_FILE}
-sqlplus -s -l $CONNECTDW @${EXTRACT_SQL} "$SCHEMA" ${FILE_NAME}
+sqlplus -s -l $CONNECTDW @${EXTRACT_SQL} ${FILE_NAME}
 cd ${DATA}
 sed -i '1d' ${FILE_NAME}
 sed -i '1i Parent_SKU|Product_Id|Product_Name|Variation_Name|SKU_Number|Primary_Category|Secondary_Category|Product_URL|Product_Image_URL|Short_Product_Desc|Long_Product_Desc|Discount|Discount_Type|Sale_Price|Retail_Price|Begin_Date|End_Date|Brand|Shipping|Is_Delete_Flag|Keywords|Is_All_Flag|Manufacturer_Name|Shipping_Information|Availablity|Universal_Pricing_Code|Class_ID|Is_Product_Link_Flag|Is_Storefront_Flag|Is_Merchandiser_Flag|Currency|Path|Group|Category|Size|Color|Larger_Images|Qty_On_Hand|AUD_Sale_Price|GBP_Sale_Price|CHF_Sale_price|CAD_Sale_Price|AU_Publish|UK_Publish|CH_Publish|CA_Publish|Order_Flag|BM_Code|Material|Full_Category_Path|Prd_Alt_Image_urls|Department_id|Clearance|Gender|Final_sale' ${FILE_NAME}
 echo "bi_partner_base Sdw  Process ended at `date '+%a %b %e %T'`" >>${LOG_FILE}
 SOURCE_COUNT=`cat ${FILE_NAME} | wc -l`
-if [ ${SOURCE_COUNT} -gt 100000 ]
-then
-echo "ftping the file as file check was succesfull `date '+%a %b %e %T'`" >>${LOG_FILE}		
-lftp -u saksoff5,tWIaOiOgZ6j94RM6WfoOfAXVxa6gRrQU sftp://sftp.sspo.com<<eof
-cd product
-put ${FILE_NAME}
-quit
-eof
-echo "End ftping the file `date '+%a %b %e %T'`" >>${LOG_FILE}		
-else
-echo "Not enough data in the product file" >>${LOG_FILE}
-echo "Simons : off5Saks.com product feed is delayed and we are looking into it"|mailx -s "Saks Product feed delayed" hbcdigtialdatamanagement@hbc.com
-exit 99
-fi
+#if [ ${SOURCE_COUNT} -gt 100000 ]
+#then
+#echo "ftping the file as file check was succesfull `date '+%a %b %e %T'`" >>${LOG_FILE}		
+#lftp -u saksoff5,tWIaOiOgZ6j94RM6WfoOfAXVxa6gRrQU sftp://sftp.sspo.com<<eof
+#cd product
+#put ${FILE_NAME}
+#quit
+#eof
+#echo "End ftping the file `date '+%a %b %e %T'`" >>${LOG_FILE}		
+#else
+#echo "Not enough data in the product file" >>${LOG_FILE}
+#echo "Simons : off5Saks.com product feed is delayed and we are looking into it"|mailx -s "Saks Product feed delayed" hbcdigtialdatamanagement@hbc.com
+#exit 99
+#fi
 #################################################################
 if [ `egrep -c "^ERROR|ORA-|not found|SP2-0|^553" ${LOG_FILE}` -ne 0 ]
 then
