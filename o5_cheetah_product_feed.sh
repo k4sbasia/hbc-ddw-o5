eff51c0a00065bd69c0a017cd83ce7f9867f2162
Jdudala <Jayanthi_dudala@s5a.com>
2020-03-31 19:37:58 -0400

    commt

---
+++ o5_cheetah_product_feed.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+#############################################################################################################################
+#####                           SAKS INC.
+#############################################################################################################################
+#####
+#####   PROGRAM NAME :o5_cheetah_product_feed.sh
+#####
+#####   DESCRIPTION  : This script does the following
+#####   CODE HISTORY :                   Name                     Date            Description
+#####                                   ------------            ----------      ------------
+#####
+#####                                   Divya Kafle             12/11/2013      Created
+#####
+#####
+#############################################################################################################################
+. $HOME/params.conf o5
+
+export SQL=$HOME/SQL
+export LOG=$HOME/LOG
+export DATA=$HOME/DATA
+export PROCESS='o5_cheetah_product_feed'
+export LOG_FILE="$LOG/${PROCESS}_log.txt"
+export JOB_NAME="${PROCESS}"
+export SCRIPT_NAME="${PROCESS}"
+export EXTRACT_SQL='$SQL/${PROCESS}.sql'
+export today=${today:-$(date +%Y%m%d)}
+export FILE_NAME="o5_cm_product_${today}.txt"
+export SFILE_SIZE=0
+export FILE_COUNT=0
+export TFILE_SIZE=0
+export SOURCE_COUNT=0
+export TARGET_COUNT=0
+export LOAD_COUNT=0
+export BAD_SUBJECT="${PROCESS} failed"
+########################################################################
+##Initialize Email Function
+########################################################################
+function send_email {
+ CURRENT_TIME=`date +"%m/%d/%Y-%H:%M:%S"`
+ cat $HOME/email_distribution_list.txt|grep '^3'|while read group address
+ do
+ cat ${LOG_FILE}|mailx -s "${SUBJECT}" $address
+ done
+}
+
+if [ "${BANNER}" == "s5a" ];
+then
+export LOG_FILE="$LOG/${PROCESS}_${BANNER}_log.txt"
+export SCHEMA="mrep."
+export PIM_PRD_ATTR_TAB="saks_all_active_pim_prd_attr"
+export PIM_SKU_ATTR_TAB="saks_all_active_pim_sku_attr"
+export PIM_ASSRT_TAB="saks_all_actv_pim_assortment"
+export PART_TABLE="BI_PARTNERS_EXTRACT_WRK"
+export BMCONNECTION="PRODSTO_MREP"
+fi
+#############################################################
+########    OFF5TH BANNER    ###############################
+############################################################
+if [ "${BANNER}" == "o5" ];
+then
+export SCHEMA="o5."
+export PART_TABLE="O5_PARTNERS_EXTRACT_WRK"
+export LOG_FILE="$LOG/${PROCESS}_${BANNER}_log.txt"
+export PIM_PRD_ATTR_TAB="pim_ab_o5_prd_attr_data"
+export PIM_SKU_ATTR_TAB="pim_ab_O5_sku_attr_data"
+export PIM_WEB_FOLDER_TAB="pim_ab_o5_web_folder_data"
+export PIM_ASRT_PRD_ASSGM="pim_ab_o5_bm_asrt_prd_assgn"
+export PIM_FOLDER_ATTR_DATA="pim_ab_o5_folder_attr_data"
+export PIM_DBLINK="PIM_READ"
+fi
+
+########Run the stats####################################################################
+sqlplus -s -l  $CONNECTDW <<EOF> ${LOG}/${PROCESS}_runstats_start.log @${SQL}/runstats_start.sql "$JOB_NAME" "$SCRIPT_NAME" "$SFILE_SIZE" "$FILE_NAME" "$LOAD_COUNT" "$FILE_COUNT" "$TFILE_SIZE" "$SOURCE_COUNT" "$TARGET_COUNT"
+EOF
+########################################################################################
+echo "Running the script to extract data started at `date '+%a %b %e %T'`" >${LOG_FILE}
+sqlplus -s -l  $CONNECTDW @${EXTRACT_SQL} "$SCHEMA" "$BANNER" "$PART_TABLE" "$PIM_DBLINK">${DATA}/${FILE_NAME}
+echo "Completed the  product extract  at `date '+%a %b %e %T'`" >>${LOG_FILE}
+########################################################################################
+TFILE_SIZE="`ls -ll $DATA/$FILE_NAME |tr -s ' ' '|' |cut -f5 -d'|'`"
+##########################################################################################
+#### FTP the file to CM
+cd $DATA
+lftp -u SDO5feeds,rEfrU8 sftp://tt.cheetahmail.com <<EOF>>${LOG_FILE}
+cd autoproc
+put ${FILE_NAME}
+quit
+EOF
+#### FTP the file to SAS
+#ftp -nv 10.130.176.210  <<EOF>>${LOG_FILE}
+user sasftp sasftp0313S
+prompt off
+bin
+#put ${FILE_NAME} o5_cm_product.txt
+quit
+EOF
+##########################################################################################
+#### Bad Records Check
+##################################################################################################
+if [ `egrep -c "^ERROR|ORA-|not found|SP2-0|^553" ${LOG_FILE}` -ne 0 ]
+then
+#mv "${LOG_FILE}" "${LOG_FILE}.`date +%Y%m%d`"
+echo "${PROCESS} failed. Please investigate"
+echo "${PROCESS} failed. Please investigate" >> ${LOG_FILE}
+export SUBJECT=${BAD_SUBJECT}
+exit 99
+#send_email
+else
+echo "${PROCESS} completed without errors."
+##Update Runstats Finish
+echo "FILE SIZE : $TFILE_SIZE" >>${LOG_FILE}
+sqlplus -s -l  $CONNECTDW<<EOF> ${LOG}/${PROCESS}_runstats_finish.log @${SQL}/runstats_end.sql "$JOB_NAME" "$SCRIPT_NAME" "$SFILE_SIZE" "$FILE_NAME" "$LOAD_COUNT" "$FILE_COUNT" "$TFILE_SIZE" "$SOURCE_COUNT" "$TARGET_COUNT"
+EOF
+echo "${PROCESS} completed without errors." >> ${LOG_FILE}
+###################################################################################################
+fi
