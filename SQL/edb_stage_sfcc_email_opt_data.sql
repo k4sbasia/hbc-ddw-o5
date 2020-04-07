61e0ed399d94f5e940be28a78d7f1ecc2971c1af
ipsingh <ishavpreet_singh@s5a.com>
2020-04-06 23:03:07 -0400

    sfcc changes

---
+++ SQL/edb_stage_sfcc_email_opt_data.sql
@@ -0,0 +1,165 @@
+SET ECHO OFF
+SET FEEDBACK on
+SET LINESIZE 10000
+SET PAGESIZE 0
+SET SQLPROMPT ''
+SET HEADING OFF
+SET TRIMSPOOL ON
+
+insert into &1.edb_stage_sub
+(
+source_id,
+email_address,
+subscribed,
+first_name,
+middle_name,
+last_name,
+address,
+address_two,
+city,
+state,
+zip_full,
+country,
+phone,
+canada_flag,
+language_id
+)
+select
+source_id,
+email_address,
+case when sub_unsub_date is null then sysdate else sub_unsub_date end,
+first_name,
+middle_name,
+last_name,
+address,
+address_two,
+city,
+state,
+zip_full,
+country,
+phone,
+canada_flag,
+language
+from
+&1.edb_stage_sfcc_email_opt_data
+where the_bay_opt_status = 'Y'
+and email_address is not null;
+
+commit;
+
+insert into &1.edb_stage_unsub
+(source_id,
+email_address,
+unsubscribed,
+first_name,
+middle_name,
+last_name,
+address,
+address_two,
+city,
+state,
+zip_full,
+country,
+phone,
+canada_flag,
+reason,
+language_id
+)
+select source_id,
+email_address,
+case when sub_unsub_date is null then sysdate else sub_unsub_date end,
+first_name,
+middle_name,
+last_name,
+address,
+address_two,
+city,
+state,
+zip_full,
+country,
+phone,
+canada_flag,
+'M',
+language
+from &1.edb_stage_sfcc_email_opt_data
+where the_bay_opt_status = 'N'
+and email_address is not null;
+
+commit;
+
+delete from
+   &1.edb_stage_sub a
+where
+   a.rowid >
+   any (select b.rowid
+   from
+      &1.edb_stage_sub  b
+   where
+        a.email_address = b.email_address
+      and trunc(a.subscribed) = trunc(b.subscribed)
+      and a.source_id in (9192,9166,9193,9184,9197,9168,9050,9173)
+      and a.source_id = b.source_id
+   )
+  and a.subscribed > (trunc(sysdate-1));
+
+commit;
+
+insert into &1.edb_sfcc_email_opt_data_hist(
+SOURCE_ID,
+EMAIL_ADDRESS,
+FIRST_NAME,
+MIDDLE_NAME,
+LAST_NAME,
+ADDRESS,
+ADDRESS_TWO,
+CITY,
+STATE,
+ZIP_FULL,
+COUNTRY, 
+PHONE, 
+OFF5TH_OPT_STATUS,
+SAKS_OPT_STATUS,    
+SAKS_CANADA_OPT_STATUS,       
+OFF5TH_CANADA_OPT_STATUS,      
+THE_BAY_OPT_STATUS,     
+SUB_UNSUB_DATE, 
+LANGUAGE,  
+BANNER,
+CANADA_FLAG,      
+SAKS_FAMILY_OPT_STATUS,     
+MORE_NUMBER,
+HBC_REWARDS_NUMBER, 
+BIRTHDAY,
+GENDER
+)
+select SOURCE_ID,
+EMAIL_ADDRESS,
+FIRST_NAME,
+MIDDLE_NAME,
+LAST_NAME,
+ADDRESS,
+ADDRESS_TWO,
+CITY,
+STATE,
+ZIP_FULL,
+COUNTRY, 
+PHONE, 
+OFF5TH_OPT_STATUS,
+SAKS_OPT_STATUS,    
+SAKS_CANADA_OPT_STATUS,       
+OFF5TH_CANADA_OPT_STATUS,      
+THE_BAY_OPT_STATUS,     
+SUB_UNSUB_DATE, 
+LANGUAGE,  
+BANNER,
+CANADA_FLAG,      
+SAKS_FAMILY_OPT_STATUS,     
+MORE_NUMBER,
+HBC_REWARDS_NUMBER, 
+BIRTHDAY,
+GENDER
+from &1.edb_stage_sfcc_email_opt_data;
+
+commit;
+
+exit
---
+++ o5_edb_extract_status_update_sfcc.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/ksh
+#############################################################################################################################
+#####                           SAKS INC.
+#############################################################################################################################
+#####
+#####   PROGRAM NAME : o5_edb_bm_extract_status_update.sh
+#####
+#####   DESCRIPTION  : This script does the following
+#####                              1.Updating the time for last Mongo/BM data pull
+#####
+#####   CODE HISTORY :  				Name                    Date            Description
+#####                                   ------------            ----------      ------------
+#####                                   Sripriya Rao           	09/16/2015      Created
+#####
+#####
+#############################################################################################################################
+################################################################
+. $HOME/initvars
+export PROCESS='o5_edb_bm_extract_status_update'
+export SQL=$HOME/SQL
+export LOG=$HOME/LOG
+export DATA=$HOME/DATA
+export LOG_FILE="$LOG/${PROCESS}_log.txt"
+export BAD_SUBJECT="${PROCESS} failed"
+export JOB_NAME="${PROCESS}"
+export SCRIPT_NAME="${PROCESS}"
+export SFILE_SIZE='0'
+export FILE_NAME='0'
+export LOAD_COUNT='0'
+export FILE_COUNT='0'
+export TFILE_SIZE='0'
+export SOURCE_COUNT='0'
+export TARGET_COUNT='0'
+export VALIDATE_COUNT='1'
+export UPDATE=''
+########################################################################
+##Initialize Email Function
+########################################################################
+function send_email {
+ CURRENT_TIME=`date +"%m/%d/%Y-%H:%M:%S"`
+ cat $HOME/email_distribution_list.txt|grep '^3'|while read group address
+ do
+ cat ${LOG_FILE}|mailx -s "${SUBJECT}" $address
+ done
+}
+#################################################################
+#################################################################
+##Update Runstats Start
+#################################################################
+echo -e "O5 EDB Mongo/BM time UPDATE process started at `date '+%a %b %e %T'`\n" >${LOG_FILE}
+#################################################################
+
+UPDATE=`sqlplus -s $CONNECTDW <<EOF
+set heading off
+update o5.edb_sub_status
+set last_extract_time = curr_extract_time,
+curr_extract_time = sysdate
+;
+commit; 
+select * from o5.edb_sub_status;
+quit;
+EOF`
+
+echo -e "O5 EDB Mongo/BM time UPDATE" >> ${LOG_FILE}
+
+#cat $UPDATE >>${LOG_FILE}
+
+echo -e "O5 EDB BM UPDATE process completed at `date '+%a %b %e %T'`\n" >> ${LOG_FILE}
+#################################################################
+# Check for errors
+################################################################
+if [ `egrep -c "^ERROR|ORA-|not found|SP2-0|^553|not connected" ${LOG_FILE}` -ne 0 ]
+then
+#mv "${LOG_FILE}" "${LOG_FILE}.`date +%Y%m%d`"
+echo -e "${PROCESS} failed. Please investigate"
+echo -e "${PROCESS} failed. Please investigate\n" >> ${LOG_FILE}
+export SUBJECT=${BAD_SUBJECT}
+#send_email
+exit 99
+else
+echo -e "${PROCESS} completed without errors."
+echo -e "${PROCESS} completed without errors.\n" >> ${LOG_FILE}
+fi
+exit $?
