c035e2e8c633ade529e6965c6bb53527cdff1d7e
Jdudala <Jayanthi_dudala@s5a.com>
2020-04-16 11:39:46 -0400

    Update rr_product_attribute_o5.sql

--- SQL/rr_product_attribute_o5.sql
+++ SQL/rr_product_attribute_o5.sql
@@ -8,7 +8,79 @@ SET PAGESIZE 0
 SET SQLPROMPT ''
 SET HEADING OFF
 SET VERIFY OFF
-SELECT p.PRODUCT_ID    ||'|'||
+SELECT 'product_id'||'|'||
+'attribute.Group'||'|'||
+'attribute.Department'||'|'||
+'attribute.cost'||'|'||
+'attribute.morecolors'||'|'||
+'attribute.preorder'||'|'||
+'attribute.rating_image_url'||'|'||
+'attribute.display_review'||'|'||
+'attribute.now_price_range'||'|'||
+'attribute.was_price_range'||'|'||
+'attribute.sale_flag'||'|'||
+'attribute.alt_image_flag'||'|'||
+'attribute.alt_image_1'||'|'||
+'attribute.alt_image_2'||'|'||
+'attribute.mobile_url'||'|'||
+'attribute.mobile_url_alt'||'|'||
+'attribute.backorderable'||'|'||
+'attribute.brandname'||'|'||
+'attribute.dropship_ind'||'|'||
+'attribute.featuredtype'||'|'||
+'attribute.gwp_flag'||'|'||
+'attribute.item_gender'||'|'||
+'attribute.pd_restrictedcountry_text'||'|'||
+'attribute.pd_restrictedstate_text'||'|'||
+'attribute.personalizable'||'|'||
+'attribute.productshortdescription'||'|'||
+'attribute.returnable'||'|'||
+'attribute.totalreviewcount'||'|'||
+'attribute.isdesigneritem'||'|'||
+'attribute.isegc'||'|'||
+'attribute.isvirtual'||'|'||
+'attribute.lifestylecontemporary'||'|'||
+'attribute.lifestylemodern'||'|'||
+'attribute.lifestylepremier'||'|'||
+'attribute.refinementproducttype1'||'|'||
+'attribute.refinementproducttype2'||'|'||
+'attribute.refinementproducttype3'||'|'||
+'attribute.refinementstyle1'||'|'||
+'attribute.refinementstyle2'||'|'||
+'attribute.refinementstyle3'||'|'||
+'attribute.refage'||'|'||
+'attribute.refcollection'||'|'||
+'attribute.refconcern1'||'|'||
+'attribute.refconcern2'||'|'||
+'attribute.refconcern3'||'|'||
+'attribute.refcoverage'||'|'||
+'attribute.refcuff'||'|'||
+'attribute.reffinefashion'||'|'||
+'attribute.reffinish'||'|'||
+'attribute.refgender'||'|'||
+'attribute.reflength'||'|'||
+'attribute.refmaterial1'||'|'||
+'attribute.refmaterial2'||'|'||
+'attribute.refmaterial3'||'|'||
+'attribute.refoccasion1'||'|'||
+'attribute.refoccasion2'||'|'||
+'attribute.refpatternprint'||'|'||
+'attribute.refrise'||'|'||
+'attribute.refspf'||'|'||
+'attribute.refscent1'||'|'||
+'attribute.refscent2'||'|'||
+'attribute.refscent3'||'|'||
+'attribute.refsilhouette1'||'|'||
+'attribute.refsilhouette2'||'|'||
+'attribute.refsilhouette3'||'|'||
+'attribute.refskintype'||'|'||
+'attribute.refsleevelength'||'|'||
+'attribute.refwash'||'|'||
+'attribute.refinementfit1'||'|'||
+'attribute.refinementfit2'||'|'||
+'attribute.refinementheelheight' FROM DUAL;
+
+SELECT distinct p.PRODUCT_ID    ||'|'||
 GROUP_ID    ||'|'||
 DEPARTMENT    ||'|'||
 COST    ||'|'||
@@ -19,6 +91,8 @@ DISPLAY_REVIEW    ||'|'||
 NOW_PRICE_RANGE    ||'|'||
 WAS_PRICE_RANGE    ||'|'||
 SALES_FLAG    ||'|'||
+O5.F_GET_RR_ALT_IMAGE (p.PRODUCT_ID, 'A1')  ||'|'||
+O5.F_GET_RR_ALT_IMAGE (p.PRODUCT_ID, 'A2')  ||'|'||
                         CASE
                            WHEN O5.F_GET_RR_ALT_IMAGE (p.PRODUCT_ID, 'A1')
                                    IS NOT NULL
@@ -28,27 +102,32 @@ SALES_FLAG    ||'|'||
                               'N'
                         END
                             ||'|'||
-                         O5.F_GET_RR_ALT_IMAGE (p.PRODUCT_ID, '')   ||'|'||
-                      O5.F_GET_RR_ALT_IMAGE (p.PRODUCT_ID, 'A1')  ||'|'||
-                      concat('https://m.saksoff5th.com/pd.jsp?productCode=', p.PRODUCT_ID)
-                           ||'|'||
-                        concat('https://image.s5a.com/is/image/saksoff5th/',p.PRODUCT_ID) ||'|'||
+concat('https://m.saksoff5th.com/pd.jsp?productCode=', p.PRODUCT_ID) ||'|'||
+concat('https://image.s5a.com/is/image/saksoff5th/',p.PRODUCT_ID) ||'|'||
 BACKORDERABLE ||'|'||
 BRANDNAME ||'|'||
 DROPSHIP_IND ||'|'||
 FEATUREDTYPE ||'|'||
 GWP_FLAG ||'|'||
-ISDESIGNERITEM ||'|'||
-ISEGC ||'|'||
-ISVIRTUAL ||'|'||
 ITEM_GENDER ||'|'||
-LIFESTYLECONTEMPORARY ||'|'||
-LIFESTYLEMODERN ||'|'||
-LIFESTYLEPREMIER ||'|'||
 PD_RESTRICTEDCOUNTRY_TEXT ||'|'||
 PD_RESTRICTEDSTATE_TEXT ||'|'||
 PERSONALIZABLE ||'|'||
 PRODUCTSHORTDESCRIPTION ||'|'||
+RETURNABLE||'|'||
+NUM_REVIEWS||'|'||
+ISDESIGNERITEM ||'|'||
+ISEGC ||'|'||
+ISVIRTUAL ||'|'||
+LIFESTYLECONTEMPORARY ||'|'||
+LIFESTYLEMODERN ||'|'||
+LIFESTYLEPREMIER ||'|'||
+REFINEMENTPRODUCTTYPE1 ||'|'||
+REFINEMENTPRODUCTTYPE2 ||'|'||
+REFINEMENTPRODUCTTYPE3 ||'|'||
+REFINEMENTSTYLE1 ||'|'||
+REFINEMENTSTYLE2 ||'|'||
+REFINEMENTSTYLE3 ||'|'||
 REFAGE ||'|'||
 REFCOLLECTION ||'|'||
 REFCONCERN1 ||'|'||
@@ -59,15 +138,6 @@ REFCUFF ||'|'||
 REFFINEFASHION ||'|'||
 REFFINISH ||'|'||
 REFGENDER ||'|'||
-REFINEMENTFIT1 ||'|'||
-REFINEMENTFIT2 ||'|'||
-REFINEMENTHEELHEIGHT ||'|'||
-REFINEMENTPRODUCTTYPE1 ||'|'||
-REFINEMENTPRODUCTTYPE2 ||'|'||
-REFINEMENTPRODUCTTYPE3 ||'|'||
-REFINEMENTSTYLE1 ||'|'||
-REFINEMENTSTYLE2 ||'|'||
-REFINEMENTSTYLE3 ||'|'||
 REFLENGTH ||'|'||
 REFMATERIAL1 ||'|'||
 REFMATERIAL2 ||'|'||
@@ -76,6 +146,7 @@ REFOCCASION1 ||'|'||
 REFOCCASION2 ||'|'||
 REFPATTERNPRINT ||'|'||
 REFRISE ||'|'||
+REFSPF ||'|'||
 REFSCENT1 ||'|'||
 REFSCENT2 ||'|'||
 REFSCENT3 ||'|'||
@@ -84,9 +155,10 @@ REFSILHOUETTE2 ||'|'||
 REFSILHOUETTE3 ||'|'||
 REFSKINTYPE ||'|'||
 REFSLEEVELENGTH ||'|'||
-REFSPF ||'|'||
 REFWASH ||'|'||
-RETURNABLE
+REFINEMENTFIT1 ||'|'||
+REFINEMENTFIT2 ||'|'||
+REFINEMENTHEELHEIGHT
 FROM
 pim_exp_bm.ALL_PRODUCT_ATTR_RR_FEED_&2@&3 prd,
                         &1.FEED_RR_PRODUCT_DAILY P
