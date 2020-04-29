b1fc052386de8f39b7741499e909be1151935af8
ipsingh <ishavpreet_singh@s5a.com>
2020-04-29 08:18:07 -0400

    cheeta url change S3Pi-373

--- SQL/o5_cheetah_product_feed.sql
+++ SQL/o5_cheetah_product_feed.sql
@@ -18,8 +18,8 @@ COMPARE_PRICE ||'|'||
 replace(MREP.CHAR_REPLACE_NEW(BRAND_NAME), '|', ' ') ||'|'||
 replace(MREP.CHAR_REPLACE_NEW(SKU_COLOR), '|', ' ') ||'|'||
 replace(MREP.CHAR_REPLACE_NEW(SKU_SIZE1_DESC), '|', '/') ||'|'||
-image_url||'_247x329.jpg'||'|'||
-image_url||'_222x296.jpg'||'|'||
+REPLACE(image_url,'300x400.jpg','_247x329.jpg')||'|'||
+REPLACE(image_url,'300x400.jpg','_222x296.jpg')||'|'||
 'USD'||'|'||
 ''
 from
